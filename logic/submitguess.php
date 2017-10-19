7f55de6bcc89e0afa39483608daccff90043edba
Adriano Gimenez <agimenez95@gmail.com>
2017-10-19 12:19:23 +0100

    small fix

--- logic/submitguess.php
+++ logic/submitguess.php
@@ -1,17 +1,12 @@
 <?php
 require '../logic/prereq.php';
-// var_dump($_POST);
-//
-// if (isset($_SESSION["userId"])) {
-//   # code...
-//   echo $_SESSION["userId"];
-// } else {
-//   echo "You did not sign in!";
-// }
 
-echo "<br>";
+//check to see if the user is logged in
+//check to see if the user has alreadd
 
 $match = new Match(getDB());
 $week = $match->getLatestWeek();
 echo $week;
+
+
 ?>
