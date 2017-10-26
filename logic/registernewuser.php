3abe3ed8a5d6f9aa62fe40e7aa577be072d6daa3
Adriano Gimenez <agimenez95@gmail.com>
2017-10-26 11:32:10 +0100

    added things

--- logic/registernewuser.php
+++ logic/registernewuser.php
@@ -3,22 +3,53 @@ require 'prereq.php';
 $username = $_POST['username'];
 $pword = $_POST['pword'];
 $pword2 = $_POST['pword2'];
+$dob = $_POST['DOB'];
+date_default_timezone_set('Europe/London');
 
-$custmanager = new CustomerManager(getDB());
-$customer = $custmanager->byUsername($username);
+//checking the dob
+// https://stackoverflow.com/questions/976669/how-to-check-if-a-date-is-in-a-given-range
+$start_date = '1917-01-01';
+check_in_range($start_date, $dob, $username, $pword, $pword2, $_POST);
+//check to see that age cannot be
 
-if ($customer){
-  echo "Username already exists";
-} else if($pword == "") {
-  echo "Empty password";
-} else if ($pword !== $pword2) {
-  echo "Passwords do not match";
-} else {
-  $customer = new Customer();
-  $customer->fromArray($_POST);
-  $customer->setPword(password_hash($pword, PASSWORD_DEFAULT));
 
-  $custmanager->save($customer);
-  header('Location: ../view/index.php');
+function check_in_range($start_date, $dob, $username, $pword, $pword2, $post) {
+  // Convert to timestamp
+  $start_ts = strtotime($start_date);
+  $end_ts = date('Y-m-d');
+  $user_ts = date('Y-m-d', strtotime($dob));
+  // Check that user date is between start & end
+  if (($user_ts < $start_ts) || ($user_ts > $end_ts)){
+    echo "problem";
+    $_SESSION['dobProb'] = 1;
+  }
+  passwordCheck($username, $pword, $pword2, $post);
 }
+
+function passwordCheck($username, $pword, $pword2, $post){
+  $custmanager = new CustomerManager(getDB());
+  $customer = $custmanager->byUsername($username);
+  if ($customer){
+    $_SESSION['userExists'] = 1;
+  } else if($pword == "") {
+    $_SESSION['pwordEmpty'] = 1;
+  } else if ($pword !== $pword2) {
+    $_SESSION['pwordMatch'] = 1;
+  } else {
+    $customer = new Customer();
+    echo $post['teamSupport'];
+    $customer->fromArray($post);
+    $customer->setPword(password_hash($pword, PASSWORD_DEFAULT));
+    $custmanager->save($customer);
+    echo "yes";
+    //header('Location: ../view/index.php');
+  }
+}
+
+
+
+
+
+
+
 ?>
--- view/registration.php
+++ view/registration.php
@@ -20,42 +20,45 @@
     <form action='../logic/registernewuser.php' method='post'>
       <tr>
         <td>First Name: </td>
-        <td><input type='text' name='firstname' /></td>
+        <td><input type='text' name='firstname' required/></td>
       </tr>
       <tr>
         <td>Surame: </td>
-        <td><input type='text' name='surname' /></td>
+        <td><input type='text' name='surname' required/></td>
       </tr>
       <tr>
         <td>Username: </td>
-        <td><input type='text' name='username' /></td>
+        <td><input type='text' name='username' required/></td>
       </tr>
       <tr>
         <td>Password: </td>
-        <td><input type='password' name='pword' /></td>
+        <td><input type='password' name='pword' required/></td>
       </tr>
       <tr>
         <td>Re-enter password: </td>
-        <td><input type='password' name='pword2' /></td>
+        <td><input type='password' name='pword2' required/></td>
       </tr>
       <tr>
         <td>Date of birth: </td>
-        <td><input type='date' name='DOB' /></td>
+        <td><input type='date' name='DOB' required/></td>
       </tr>
       <tr>
         <td>Email address: </td>
-        <td><input type='text' name='email' /></td>
+        <td><input type='email' name='email' required/></td>
       </tr>
       <tr>
         <td>Favourite team: </td>
-        <td>   <?php echo "<select name='bonusPlayer'>";
-							$tm = new TeamManager(getDB());
-							$allTeams = $tm->showAllOptions();
-							foreach ($allTeams as $value) {
-								$punditsFaveTeam = $tm->byID($value);
-								echo "<option value =".$punditsFaveTeam.">$value</option>";
-							}
-						echo "</select>"; ?></td>
+        <td>
+					<?php echo "<select name='teamSupport'>";
+					$tm = new TeamManager(getDB());
+					$allTeams = $tm->showAllOptions();
+					foreach ($allTeams as $value) {
+						$punditsFaveTeam = $tm->byID($value);
+						echo "<option value ='$punditsFaveTeam'>$value</option>";
+					}
+					echo "</select>";
+					?>
+					</td>
       </tr>
       <tr>
         <td><input type='submit' name='submit'/></td>
