0d0c98733a4f274e7692f657c368007746ac1b07
Adriano Gimenez <agimenez95@gmail.com>
2017-10-26 14:32:15 +0100

    rules added to the index page

---
+++ view/rulesofthegame.php
@@ -0,0 +1,6 @@
+<h1>Rules of the Game</h1>
+<h3>Requirements</h3>
+<p>To be able to enter to play this game you must register with a valid account to be able to make your predictions.</p>
+<p>You can only access the game once you have registered.</p>
+<h3>Terms and Conditions</h3>
+<p>In this version of the game we are not liable to give you the money as this is not for commercial use.</p>
