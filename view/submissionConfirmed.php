4ef7e62d60bbe1147b1e76c3399843dfe4ffcd69
Adriano Gimenez <agimenez95@gmail.com>
2017-10-27 08:23:33 +0100

    page added for prediction confirmation

---
+++ view/submissionConfirmed.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>THANK YOU FOR YOUR PREDICTION. WE WILL BE IN TOUCH IF YOU WIN.</h1>
+    <a href="index.php">Home</a>
+  </body>
+</html>
